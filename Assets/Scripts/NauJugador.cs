5bd0af0c945003c39608bf9c489ee5323319d7e7
CER-ArnauRP <a.rodriguez@ceroca.cat>
2024-09-19 19:32:07 +0200

    Control límits pantalla

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,21 @@ using UnityEngine;
 public class NauJugador : MonoBehaviour
 {
     private float _vel;
+
+    private Vector2 minPantalla, maxPantalla;
     
     // Start is called before the first frame update
     void Start()
     {
         _vel = 8;
+
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        minPantalla.x = minPantalla.x + 0.75f;
+        maxPantalla.x = maxPantalla.x - 0.75f;
+        minPantalla.y = minPantalla.y + 0.75f;
+        maxPantalla.y = maxPantalla.y - 0.75f;
     }
 
     // Update is called once per frame
@@ -23,6 +34,10 @@ public class NauJugador : MonoBehaviour
 
         Vector2 novaPos = transform.position;
         novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
+
+        novaPos.x = Mathf.Clamp(novaPos.x, minPantalla.x, maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
+        
         transform.position = novaPos;
     }
 }
